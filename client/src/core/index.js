6f6c560d9f745680506cd98f0d1488649d02a359
unknown <faizeraza2468@gmail.com>
2022-05-26 00:50:41 +0500

    changingUrl

--- client/src/core/index.js
+++ client/src/core/index.js
@@ -3,7 +3,7 @@ var url = window.location.href.split(":");
 if (url[0]==="https")
 {
     // url = "https://databaselogin.herokuapp.com"
-    url = "https://sweet-server.herokuapp.com"
+    url = "https://sweet-server.herokuapp.com/"
 }
 else{
     url = "http://localhost:5000"
